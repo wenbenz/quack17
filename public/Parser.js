cd2fad7a7d16e9654bc971b9bf6c6fe9681e6cdb
Alexei Tipenko <atipenko2@gmail.com>
2017-02-04 01:40:46 -0500

    Starting making parsing functions

--- public/Parser.js
+++ public/Parser.js
@@ -1,17 +1,44 @@
+
+
 $(document).ready(function() {
+  $("#submitButton").click(function() {
+    spaceParser();
+    avgSentenceLen();
+  });
+});
 
 
-  var arr = spaceParser("Oh hey my name is Max");                    //Start a new game
-  for (int i=0; i<arr.length; i++) {
-    console.log(arr[i]);
+function spaceParser() {
+  var string = $("#textBox").val();
+  var wordsArray = string.split(" ");
+
+  for (var i=0; i < wordsArray.length; i++) {
+    console.log(wordsArray[i]);
+  }
+}
+
+function avgSentenceLen() {
+  var string = $("#textBox").val();
+  var sentenceArray = string.split(".");
+  console.log(sentenceArray);
+  var sentenceLenArr = [];
+  var sum = 0;
+  console.log(sum);
+
+  for (var i=0; i<sentenceArray.length; i++) {
+    var tempSentence = sentenceArray[i].split(" ");
+
+    if (tempSentence[0] == " ")
+      tempSentence = tempSentence.subtring(1);
+
+    sentenceLenArr[i] = tempSentence.length;
+    sum += sentenceLenArr[i];
+    console.log(sum);
   }
 
-
-
-  /*Creates new empty board */
-  /*input: data, output: new game board (not directly returned)*/
-  function spaceParser(data) {
-    var wordsArray = data.split(" ");
-    return wordsArray;
-  }
+  console.log("***");
+  console.log(sum);
+  console.log(sentenceArray.length);
+  var avgLen = sum / sentenceArray.length;
+  console.log(avgLen);
 }
