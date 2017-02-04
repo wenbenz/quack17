3224d2ca716357a8fc1a665e3953e8e7e1c4264f
Avery Vine <averyvine@cmail.carleton.ca>
2017-02-04 13:31:37 -0500

    basic example of Scales.js use

--- public/Scales.js
+++ public/Scales.js
@@ -1,10 +1,10 @@
 var scaleNotes = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"];
-var majorIndices = {0, 2, 4, 5, 7, 9, 11};
-var minorIndices = {0, 2, 3, 5, 7, 8, 10};
-var pentatonicIndices = {0, 2, 4, 7, 9};
+var majorIndices = [0, 2, 4, 5, 7, 9, 11];
+var minorIndices = [0, 2, 3, 5, 7, 8, 10];
+var pentatonicIndices = [0, 2, 4, 7, 9];
 
 function getScale(root, octave, scale) { //root note in numbers, octave in number,
-  var indices, returnedScale;
+  var indices, returnedScale = [];
   if (scale === "minor") {
     indices = minorIndices;
   }
