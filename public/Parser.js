33c743a6e4e570ea9709ddbbf21c2046bd9f46b9
Avery Vine <averyvine@cmail.carleton.ca>
2017-02-04 10:03:10 -0500

    fixed parseInput() bug

--- public/Parser.js
+++ public/Parser.js
@@ -1,32 +1,29 @@
 var sentences = [];
 
-$(document).ready(function() {
-  function parseInput() {
-    console.log("Button is clicked");
-    var textInput = $("#textBox").val();                  // Retrieve the input from the webpage
-    textInput = textInput.replace(".", "./~");
-    textInput = textInput.replace("!", "!/~");
-    textInput = textInput.replace("?", "?/~");
-    var sentenceArray = textInput.split("/~");           // Split the input into an array of sentences
-    sentenceArray.pop();
-    for (var i = 0; i < sentenceArray.length; i++) {                            // Loop through every sentence
-      while (sentenceArray[i].substring(0, 1) === " ") {  // While there is whitespace in front of the sentence, remove it
-        sentenceArray[i] = sentenceArray[i].substring(1);
-      }
-      var words = [];
-      var wordArray = sentenceArray[i].split(" ");        // Split the current sentence into an array of words
-      for (var j = 0; j < wordArray.length; j++) {                              // Loop through every word
-        var syllables = [];
-        for (var k = 0; k < wordArray[j].length; k++){
-          syllables = getSylArray(wordArray[j], [], "");
-        }
-        words.push(syllables);                         // Push the array of syllables into the current word
-      }
-      sentences.push(words);                           // Push the array of words into the current sentence
+function parseInput() {
+  var textInput = $("#textBox").val();                  // Retrieve the input from the webpage
+  textInput = textInput.replace(".", "./~");
+  textInput = textInput.replace("!", "!/~");
+  textInput = textInput.replace("?", "?/~");
+  var sentenceArray = textInput.split("/~");           // Split the input into an array of sentences
+  sentenceArray.pop();
+  for (var i = 0; i < sentenceArray.length; i++) {                            // Loop through every sentence
+    while (sentenceArray[i].substring(0, 1) === " ") {  // While there is whitespace in front of the sentence, remove it
+      sentenceArray[i] = sentenceArray[i].substring(1);
     }
-    return sentences;
+    var words = [];
+    var wordArray = sentenceArray[i].split(" ");        // Split the current sentence into an array of words
+    for (var j = 0; j < wordArray.length; j++) {                              // Loop through every word
+      var syllables = [];
+      for (var k = 0; k < wordArray[j].length; k++){
+        syllables = getSylArray(wordArray[j], [], "");
+      }
+      words.push(syllables);                         // Push the array of syllables into the current word
+    }
+    sentences.push(words);                           // Push the array of words into the current sentence
   }
-});
+  return sentences;
+}
 
 function getSylArray(word,arr,syl){
   var first = word.substring(0,1);
