930322c419158b1ef263c44bbd5a9ff14fab94ba
Avery Vine <averyvine@cmail.carleton.ca>
2017-02-04 11:30:16 -0500

    Initial beat loop

--- public/Music.js
+++ public/Music.js
@@ -1,25 +1,38 @@
 var synth = new Tone.Synth();
 var vol = new Tone.Volume(-10);
+var beatLoop;
+var barCounter;
+var timeSignature = 4;
 synth.chain(vol, Tone.Master); //chain events
 
 function prepareBeat(input) {
-  synth.triggerAttackRelease("C4", "4n");
-  var tempo = parseTempo(input);
-  console.log("Avg Length: " + tempo);
-  loopBeat(tempo);
+  var bpm;
+  $.getScript("Tempo.js", function() {
+    bpm = parseTempo(input);
+    console.log("BPM: " + bpm);
+    barCounter = 0;
+    loopBeat(bpm);
+  });
 }
 
 function loopBeat(bpm) {
-
+  beatLoop = setInterval(beat, 60000 / bpm);
 }
 
-function parseTempo(input) {
-  var avgLength = 0;
-  for (i in input) {
-    avgLength += input[i].length;
+function beat() {
+  if (barCounter == 0) {
+    synth.triggerAttackRelease("D4", "4n");
   }
-  avgLength /= input.length;
-  return avgLength;
+  else {
+    //Do a small thunk
+    synth.triggerAttackRelease("C4", "4n");
+  }
+  barCounter++;
+  barCounter %= timeSignature;
+}
+
+function stop() {
+  clearInterval(beatLoop);
 }
 
 function adjustVolume(data) {
